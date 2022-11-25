e5da1382bb73017c5af38f574641f627e18f286a
gibbo101 <lukegibson101@gmail.com>
2022-11-25 12:43:24 +0000

    create

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Role;
+use App\Models\User;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +18,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create', function() {
+  $user = User::find(1);
+
+  $role = new Role(['name'=>'Administrator']);
+
+  $user->roles()->save($role);
+
 });
